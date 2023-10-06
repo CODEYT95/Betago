47da75d4904376d7b7593d6b6cf323c665c3179c
yunji6393 <lyj9594@naver.com>
2023-10-06 19:07:46 +0900

    10/6-lyj

--- src/main/resources/static/js/learning/task/student/submitTask.js
+++ src/main/resources/static/js/learning/task/student/submitTask.js
@@ -1,25 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const checkboxes = document.querySelectorAll('.checkbox-input');
-    const paymentButton = document.getElementById('paymentButton');
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitButton");
+  const checkboxes = document.querySelectorAll(".checkbox-input");
 
-    // 체크박스 중 하나라도 체크되어 있다면 true를 반환, 아니면 false를 반환
-    function isAnyCheckboxChecked() {
-        return Array.from(checkboxes).some(checkbox => checkbox.checked);
-    }
+  // 체크박스 상태 확인 및 버튼 활성화/비활성화
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function () {
+      let isAnyCheckboxChecked = false;
 
-    // 체크박스의 상태에 따라 버튼 활성화/비활성화 설정
-    function updatePaymentButtonState() {
-        if (isAnyCheckboxChecked()) {
-            paymentButton.removeAttribute('disabled');
-        } else {
-            paymentButton.setAttribute('disabled', 'disabled');
+      checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+          isAnyCheckboxChecked = true;
         }
-    }
+      });
 
-    // 체크박스 상태 변경 시 함수 호출
-    checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener('change', updatePaymentButtonState);
+      if (isAnyCheckboxChecked) {
+        submitBtn.removeAttribute("disabled");
+      } else {
+        submitBtn.setAttribute("disabled", "disabled");
+      }
     });
-    // 초기 로드 시 한번 확인
-    updatePaymentButtonState();
+  });
+
+    // 숙제하기 버튼에 클릭 이벤트 핸들러 추가
+    var selectButtons = document.querySelectorAll(".selectBtn");
+
+    selectButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // 숙제하기 버튼을 클릭한 경우
+            var modal = document.getElementById("myModal"); // 모달 요소 가져오기
+            var modalBg = document.querySelector(".modal-bg"); // 모달 배경 요소 가져오기
+            var tasksendNo = button.getAttribute("data-tasksend-no"); // 데이터 속성에서 숙제 번호 가져오기
+
+            console.log(tasksendNo);
+            // 모달 내부에 숙제 번호 표시
+            var modalTaskNo = document.querySelector("#myModal input[name='tasksend_no']");
+            modalTaskNo.value = tasksendNo;
+
+            // 모달과 모달 배경을 보이게 설정
+            modal.style.display = "block";
+            modalBg.style.display = "block";
+        });
+    });
+
+    // 모달 닫기 버튼에 이벤트 리스너 추가
+    var closeModalButtons = document.querySelectorAll(".conceal");
+    closeModalButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var modal = document.getElementById("myModal"); // 모달 요소 가져오기
+            var modalBg = document.querySelector(".modal-bg"); // 모달 배경 요소 가져오기
+
+            // 모달과 모달 배경을 숨기게 설정
+            modal.style.display = "none";
+            modalBg.style.display = "none";
+        });
+    });
+
 });
