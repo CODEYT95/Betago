063755b965d29a18804fdec8c200de20e302cd51
uu940903 <uu940903@naver.com>
2023-10-09 20:14:25 +0900

    10/09 드래그앤드롭 하는 중

--- src/test/java/com/bnw/beta/BetaApplicationTests.java
+++ src/test/java/com/bnw/beta/BetaApplicationTests.java
@@ -1,10 +1,9 @@
 package com.bnw.beta;
 
+import com.bnw.beta.domain.admin.dto.NoticeDTO;
+import com.bnw.beta.service.admin.notice.NoticeServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 @SpringBootTest
 class BetaApplicationTests {
