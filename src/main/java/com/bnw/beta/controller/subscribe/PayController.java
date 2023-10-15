af16c4440a7d243be9fc08f47216f9cfb28502b6
chdohun <138775181+chdohun@users.noreply.github.com>
2023-10-15 17:22:30 +0900

    Update PayController.java

--- src/main/java/com/bnw/beta/controller/subscribe/PayController.java
+++ src/main/java/com/bnw/beta/controller/subscribe/PayController.java
@@ -6,56 +6,42 @@ import com.bnw.beta.service.subscribe.pay.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
+@RequestMapping("pay")
 public class PayController {
     @Autowired
     private PayService payService;
 
-    /*
-    @PostMapping(value = "/addCart")
-    public String addToCart(@RequestParam("selectedGameNos") int[] selectedGameNos, String member_id) {
-        member_id="dumy";
-        StringBuilder redirectUrl = new StringBuilder("redirect:/cartList?");
-        for (int gameNo : selectedGameNos) {
-            redirectUrl.append("GameNos=").append(gameNo);
-
-            payService.insertIntoCart(gameNo, member_id);
-        }
-
-        // "/cartList"로 바로 리다이렉트
-        return "redirect:/cartList";
-    }
-*/
-    //제가 한 컨트롤러
     @GetMapping("/cartList")
     public String cartList(Model model, @RequestParam("game_no") List<Integer> game_no) {
         List<payDTO> cartlist = payService.selectBuylist(game_no);
+        System.out.println("test"+cartlist);
         model.addAttribute("cartlist", cartlist);
         return "subscribe/pay";
     }
 
     @PostMapping("/payment")
     public String submitPay(@RequestParam("game_no[]") List<Integer> game_no,
-                            payDTO payDTO, String member_id) {
-
-        member_id="baduk";
+                            @RequestParam("game_date[]") List<String> game_date,
+                            @RequestParam("pay_type") String pay_type,
+                            payDTO payDTO, Principal principal) {
+        System.out.println("테스트"+pay_type);
+        String member_id = principal.getName();
 
         for (int i = 0; i < game_no.size(); i++) {
             Integer gameNo = game_no.get(i);
-
+            String gameDate = game_date.get(i);
             payDTO.setMember_id(member_id);
             payDTO.setGame_no(gameNo);
-
+            payDTO.setGame_date(gameDate);
             System.out.println(payDTO);
             payService.insertIntoPay(payDTO);
         }
-        return "redirect:/subscribe/paylist";
+        return "redirect:/game/list";
     }
 }
