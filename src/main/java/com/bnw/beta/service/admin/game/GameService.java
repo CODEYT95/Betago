5cc3ac1bdd8a11a3146c88f926875dd620e39d98
dohun <chdohun0614@naver.com>
2023-10-19 10:37:13 +0900

    10/18 구독하기와 조회하기

--- src/main/java/com/bnw/beta/service/admin/game/GameService.java
+++ src/main/java/com/bnw/beta/service/admin/game/GameService.java
@@ -18,14 +18,17 @@ public interface GameService {
     int insertGameImage(GameFileDTO gameFileDTO);
 
     //게임콘텐츠 조회
-   public List<GameDTO> selectAll();
+    List<GameDTO> selectGameList(String game_title, int limit, int offset);
+
+    //게임콘텐츠 제목 조회
+    List<GameDTO> selectGameTitle();
+
+    //게임콘텐츠 갯수
+    int countGameList(String game_title);
 
     //월간 (일일 단위 매출조회)
     public List<GameDTO> selectDailySales(Date game_startsearch, Date game_endsearch);
 
     //년간 (월 단위 매출조회)
     public List<GameDTO> selectMonthlySales(Date game_startsearch, Date game_endsearch);
-
-    //게임콘텐츠 제목검색
-    public List<GameDTO> searchByTitle(@Param("game_title") String game_title);
 }
