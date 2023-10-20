aff1c1f925b2e13237630a87f69dde0cb0005170
park <codeyt95@gmail.com>
2023-10-20 11:13:16 +0900

    10/12 가입승인 페이지 진행중

--- src/main/java/com/bnw/beta/config/post/NoticeConfig.java
+++ src/main/java/com/bnw/beta/config/post/NoticeConfig.java
@@ -4,10 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class NoticeConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
+
+        String projectDir = System.getProperty("user.dir"); // 현재 프로젝트 디렉토리 가져오기
+        Path uploadPath = Paths.get(projectDir, "src", "main", "resources", "static", "image", "guide", "game");
+
+
         registry.addResourceHandler("/image/notice/**")
                 .addResourceLocations("file:///c:/uploadfile/notice_img/");
     }
