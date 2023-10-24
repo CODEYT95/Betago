e25e5b591a1a783f0efd1e5042ceedaf9d172c89
park <codeyt95@gmail.com>
2023-10-24 09:15:09 +0900

    10/12 가입승인 페이지 진행중

--- src/main/java/com/bnw/beta/service/learning/group/GroupService.java
+++ src/main/java/com/bnw/beta/service/learning/group/GroupService.java
@@ -42,6 +42,9 @@ public interface GroupService {
     //학습 그룹 수정
     int updateGroup(GroupDTO groupDTO);
 
+    //그룹 등록 가능한 인원수 구하기 위한 쿼리
+    int currentGroupCnt(int group_no, int game_no, String member_id);
+
     //그룹 학생 가입 승인 목록
     GroupPageDTO selectGroupApprove(@Param("member_id") String member_id, @Param("group_no") Integer group_no, @Param("page") int page, @Param("size") int size);
 
